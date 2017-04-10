c1888f243e4f4cab57a137680bc2a5af790917ef
jessehartloff <hartloff@buffalo.edu>
2017-04-10 17:17:41 -0400

    initial commit

---
+++ src/PS10_JSON.java
@@ -0,0 +1,132 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PS10_JSON{
+
+
+    static String Q1(int a, int b, int c){
+        // return a JSON String that is an array containing the values a, b, and c in that order.
+
+        return "";
+    }
+
+
+    static String Q2(ArrayList<Integer> numbers){
+        // return a JSON String that is an array containing the values of the input ArrayList in the same
+        // order as they appear in the ArrayList.
+
+        return "";
+    }
+
+
+    static String Q3(String key, int value){
+        // return a JSON String that is an object containing the provided key-value pair
+
+        return "";
+    }
+
+
+    static String Q4(String key, String value){
+        // return a JSON String that is an object containing the provided key-value pair
+
+        return "";
+    }
+
+
+    static String Q5(HashMap<String, Integer> input){
+        // return a JSON String that is an object containing the same key-value pairs stored in the input HashMap
+
+        return "";
+    }
+
+
+    static String Q6(String json){
+        // given a JSON object in the format {"title":"Fight Club","tagline":"Mischief. Mayhem. Soap.","year":1999},
+        // return the value at the key "title" as a String
+
+        return "";
+    }
+
+
+    static String Q7(String json){
+        // given a JSON object in the format {"title":"Fight Club","tagline":"Mischief. Mayhem. Soap.","year":1999},
+        // return the value at the key "tagline" as a String
+
+        return "";
+    }
+
+
+    static int Q8(String json){
+        // given a JSON object in the format {"title":"Fight Club","tagline":"Mischief. Mayhem. Soap.","year":1999},
+        // return the value at the key "year" as a int
+
+        return 0;
+    }
+
+
+    static String Q9(String json){
+        // given a JSON String that is an array in this format [5,"airplane","volunteer","nope"],
+        // return data at index 2 as a String
+
+        return "";
+    }
+
+
+    static ArrayList<String> Q10(String json){
+        // given a JSON String that is an array containing an unknown number of Strings,
+        // return all the values in the array in an ArrayList of Strings
+
+        return null;
+    }
+
+
+    static String Q11(String json){
+        // given a JSON String in the format
+        // [{"title":"The Matrix","year":1999},{"title":"The Shawshank Redemption","year":1994}]
+        // return the value at the key "title" of the second object in the array.
+
+        return "";
+    }
+
+
+    static int Q12(String json){
+        // given a JSON String in the format of an array of objects where each object is in the format
+        // {"title":"Star Wars: Episode V - The Empire Strikes Back","year":1980}
+        // return the number of ojects in the array.
+
+        return 0;
+    }
+
+
+    static ArrayList<Integer> Q13(String json){
+        // given a JSON String in the format of an array of objects where each object is in the format
+        // {"title":"Pulp Fiction","year":1994}
+        // return the values at the key "year" for all objects in the array in an ArrayList of Integers
+
+        return null;
+    }
+
+
+    static ArrayList<String> Q14(String json){
+        // given a JSON String in the format of an array of objects where each object is in the format
+        // {"title":"Inception","year":2010,"stars":["Leonardo DiCaprio","Joseph Gordon-Levitt","Ellen Page"]}
+        // return all the values at the key "stars" for all objects in the array in an ArrayList of Strings.
+        // The value stored at "stars" will be an array for each object.
+
+        return null;
+    }
+
+
+    static String Q15(String json, String key, String value){
+        // given a JSON String in the form of a JSON object, add the the provided key-value pair to this
+        // object and return it as a JSON String.
+
+        return "";
+    }
+
+
+
+    public static void main(String[] args){
+
+    }
+}
